def1610cc8abeb93c4688b57f2b2163af13b9dea
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-12 20:05:36 +0700

    update

--- laravel/laravel-app-01/app/Http/Controllers/TestController.php
+++ laravel/laravel-app-01/app/Http/Controllers/TestController.php
@@ -3,13 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 
 class TestController extends Controller
 {
-    public function index(){
-        if(App){
-            return "App Name: ..."
+    public function index()
+    {
+        if (App::environment("local")) {
+            return "App Name: ..." . config("app.name");
         }
+        return "Your environment is not local!";
     }
-    //
+
+    public function configTest()
+    {
+        Config::set('app.timezone', 'American/Chicago');
+
+        $bad = null;
+
+        try {
+            Config::integer('app.timezone');
+        } catch (\Throwable $th) {
+            $bad = $th->getMessage();
+        }
+
+        return response()->json([
+            'get' => config('app.timezone'),
+            'string(app.name)' => Config::string('app.name')
+        ]);
+    }
 }
