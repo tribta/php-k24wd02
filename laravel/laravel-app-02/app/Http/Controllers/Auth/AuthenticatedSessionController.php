416408ac73415eb8a5b0fd15d361ae6fdc37d010
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-19 20:01:18 +0700

    upodate

--- laravel/laravel-app-02/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ laravel/laravel-app-02/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AuthenticatedSessionController extends Controller
 {
-    public function create() {}
+    public function create(): Response
+    {
+        return Inertia::render("auth/Login");
+    }
     public function store() {}
     public function destroy() {}
 }
--- laravel/laravel-app-02/app/Http/Controllers/Auth/RegistedUserController.php
+++ laravel/laravel-app-02/app/Http/Controllers/Auth/RegistedUserController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class RegistedUserController extends Controller
 {
-    public function create() {}
-    public function store() {}
+    public function create(): Response
+    {
+        return Inertia::render("auth/Register");
+    }
+    public function store(Request $request): RedirectResponse
+    {
+        $data = $request->validate([
+            "name" => ["required", "string", "max:255"],
+            "email" => ["required", "string", "max:255", "email", "unique:users,email"],
+            "password" => ["required", "string", "min:6", "confirm"]
+        ]);
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => Hash::make($data["password"])
+        ]);
+        Auth::login($user);
+        return redirect()->intended("/dashboard")->with("success", "Register Successfully!");
+    }
 }
