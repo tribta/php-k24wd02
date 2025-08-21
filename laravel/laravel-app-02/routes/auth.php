30b376d56835e9488902c0da5ee86d558c185206
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-21 20:47:19 +0700

    update

--- laravel/laravel-app-02/routes/auth.php
+++ laravel/laravel-app-02/routes/auth.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegistedUserController;
+use App\Http\Controllers\TransactionController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("guest")->group(function () {
@@ -12,4 +15,20 @@ Route::middleware("guest")->group(function () {
     Route::post("/register", [RegistedUserController::class, "store"]);
 });
 
-Route::post("/logout", [AuthenticatedSessionController::class, "destroy"])->middleware("auth");
+Route::middleware("auth")->group(function () {
+    Route::post("/logout", [AuthenticatedSessionController::class, "destroy"]);
+
+    // Account
+    Route::get("/accounts", [AccountController::class, "index"]);
+    Route::get("/accounts/create", [AccountController::class, "create"]);
+    Route::post("/accounts", [AccountController::class, "store"]);
+
+    // Transaction
+    Route::get("/accounts/{account}/transactions", [TransactionController::class, "index"]);
+
+    Route::get("/accounts/{account}/deposit", [TransactionController::class, "createDeposit"]);
+    Route::post("/accounts/{account}/deposit", [TransactionController::class, "storeDeposit"]);
+
+    Route::get("/accounts/{account}/withdraw", [TransactionController::class, "createWithdraw"]);
+    Route::post("/accounts/{account}/withdraw", [TransactionController::class, "storeWithdraw"]);
+});
