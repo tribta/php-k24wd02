795bb5062f63ce9faf2318e2cc51a17f002b5190
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-21 21:04:56 +0700

    update

--- laravel/laravel-app-02/resources/js/pages/Accounts/Index.jsx
+++ laravel/laravel-app-02/resources/js/pages/Accounts/Index.jsx
@@ -1,5 +1,45 @@
-function Index() {
-    return <div>Index</div>;
+import { Link, usePage } from '@inertiajs/react';
+import MainLayout from '../layouts/MainLayout';
+
+function Index({ accounts }) {
+    const { props } = usePage();
+    return (
+        <MainLayout>
+            <div className="card">
+                <div>
+                    <h2>My Accounts</h2>
+                    <Link className="btn" href={'/accounts/create'}>
+                        + Create New Account
+                    </Link>
+                </div>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>number</th>
+                            <th>name</th>
+                            <th>balance</th>
+                            <th>transaction</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {accounts.map((a) => (
+                            <tr>
+                                <td>{a.number}</td>
+                                <td>{a.name}</td>
+                                <td>{a.balance}</td>
+                                <td>
+                                    <Link href={`/accounts/${a.id}/transactions`}>
+                                        Transaction
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </MainLayout>
+    );
 }
 
 export default Index;
