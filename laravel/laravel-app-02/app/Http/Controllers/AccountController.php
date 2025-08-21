2988bc62bff92b08c5981272323c162527584fe2
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-21 20:10:17 +0700

    update

---
+++ laravel/laravel-app-02/app/Http/Controllers/AccountController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Support\Str;
+
+class AccountController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        $accounts = Account::query()
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->get(['user_id', 'number', 'name', 'currency', 'balance']);
+
+        return Inertia::render('Accounts/Index', ['accounts' => $accounts]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('Accounts/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:100'],
+            'currency' => ['required', 'string', 'size:3']
+        ]);
+        $number = 'AC' . strtoupper(Str::random(10));
+        $account = Account::create([
+            'user_id' => $request->user()->id,
+            'number' => $number,
+            'name',
+            'currency',
+            'balance'
+        ]);
+        return redirect("accounts/{$account->id}/transaction")->with("success", "Your
+        bank account has been created! Please deposit balance to start!.");
+    }
+}
