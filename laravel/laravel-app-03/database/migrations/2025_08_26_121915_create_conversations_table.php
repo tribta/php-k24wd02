a4e399d6cb6f15db0200fb06b700bff45e2e0f18
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-26 19:33:24 +0700

    update

---
+++ laravel/laravel-app-03/database/migrations/2025_08_26_121915_create_conversations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('name')->nullable();
+            $table->boolean('is_direct')->default(true);
+            $table->foreignId('created_by')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->index(['is_direct', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conversations');
+    }
+};
