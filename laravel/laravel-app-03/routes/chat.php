22f7e9aab91e5587ca4c249614f2a2ff1ba4c46c
Gabriel7721 <176547965+tribta@users.noreply.github.com>
2025-08-28 20:42:44 +0700

    update route

--- laravel/laravel-app-03/routes/chat.php
+++ laravel/laravel-app-03/routes/chat.php
@@ -6,10 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("auth")->group(function () {
     // tải về danh sách conversations
     Route::get('/chat', [ConversationController::class, 'index'])->name('chat.index');
+
     // xem chi tiết 1 conversation
-    Route::get('/chat/{conversation}');
+    Route::get(
+        '/chat/{conversation}',
+        [ConversationController::class, 'show']
+    )->name('chat.show');
+
     // tạo mới 1 conversation
-    Route::post('/chat')->name('chat.show');
+    Route::post('/chat', [ConversationController::class, 'store'])
+        ->name('chat.store');
+
     // gửi 1 message
     Route::post('/messages');
 });
